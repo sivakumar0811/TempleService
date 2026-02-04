efa8311c020a76ac158820d09e89f7148643f026
sivakumartuta <sivakumartuta10@gmail.com>
2026-02-05 01:22:22 +0530

    Remove

--- src/main/java/com/tutasi/AmmavariGudiServiceApplication.java
+++ src/main/java/com/tutasi/AmmavariGudiServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AmmavariGudiServiceApplication {
 
 	public static void main(String[] args) {
-		// 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 // added
 		SpringApplication.run(AmmavariGudiServiceApplication.class, args);
 	}
 
