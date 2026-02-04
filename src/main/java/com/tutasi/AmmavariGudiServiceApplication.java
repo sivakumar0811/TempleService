aa53c1564bded188cd5e56f13de645bc0d69ef6b
sivakumartuta <sivakumartuta10@gmail.com>
2026-02-05 01:06:44 +0530

    logger

--- src/main/java/com/tutasi/AmmavariGudiServiceApplication.java
+++ src/main/java/com/tutasi/AmmavariGudiServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AmmavariGudiServiceApplication {
 
 	public static void main(String[] args) {
-		// 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
+		// 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 // added
 		SpringApplication.run(AmmavariGudiServiceApplication.class, args);
 	}
 
