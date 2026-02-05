10ced592e99e74767a680f57b8bce83c6e2bef34
sivakumartuta <sivakumartuta10@gmail.com>
2026-02-05 12:27:26 +0530

    commit 1

---
+++ src/main/java/com/tutasi/AmmavariGudiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tutasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmmavariGudiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmmavariGudiServiceApplication.class, args);
+	}
+
+}
